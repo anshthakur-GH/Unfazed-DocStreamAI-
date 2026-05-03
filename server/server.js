20aec6471743ccae801a4ceba31d6235b5eff1b2
anshthakur--gh <mailtodroidoox@gmail.com>
2026-05-04 00:23:11 +0530

    fix: remove dotenv override so Render env vars take precedence

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-require('dotenv').config({ override: true });
+require('dotenv').config();
 
 // Import custom modules
 const connectDB = require('./config/database');
